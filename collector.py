0d36558465626a21e73ad10b2628cbd8461e6136
leonov-av <alexander.v.leonov@gmail.com>
2019-08-24 11:22:02 +0300

    Get POST requests to https://your_zbrunk_host:8088/services/collector, print headers and data

--- collector.py
+++ collector.py
@@ -3,13 +3,26 @@
 # We need here a flask-based service that will get events from user and put them in Mongodb
 
 # pip3 install flask
-from flask import Flask
+
+from flask import Flask, request
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return "Hello, it's Zbrunk!"
+
+
+@app.route('/services/collector', methods=['POST'])
+def collect():
+    if request.method == 'POST':
+        headers = request.headers
+        data = request.form # a multidict containing POST data
+        print(headers)
+        print(data)
+        return '{"text":"Success","code":0}'
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8088, ssl_context=('cert/certificate.pem', 'cert/key.pem'), debug=True)
