35af6cef8c949b09a27b0474835126028a52dea9
leonov-av <alexander.v.leonov@gmail.com>
2019-12-08 07:18:21 +0300

    Search get_types

--- web/searcher.py
+++ web/searcher.py
@@ -17,7 +17,6 @@ def search():
         print(data_string)
         events = list()
 
-
         # Example:
         #curl -k https://127.0.0.1:8088/services/searcher \
         # -d '{"search": "query", "output_mode": "json", "max_count":"10000000",
@@ -55,16 +54,28 @@ def search():
                         code = 2
                         status_ok = False
 
+        get_types = False #requeres read reprmissions
+        if 'get_types' in search_params:
+            if search_params['get_types'] == "True":
+                get_types = True
+
         if status_ok == True:
             text = "Success"
             events = search_params['search']
             code = 0
 
-            search_request = {"$and": [
+            if get_types:
+                search_request = {"$and": [
+                        {"time": {"$gte": int(search_params['search']["time"]["from"])}},
+                        {"time": {"$lte": int(search_params['search']["time"]["to"])}}
+                ]}
+            else:
+                search_request = {"$and": [
                     {"event_type": {"$eq": search_params['search']["event_type"]}},
                     {"time": {"$gte": int(search_params['search']["time"]["from"])}},
                     {"time": {"$lte": int(search_params['search']["time"]["to"])}}
-            ]}
+                ]}
+
 
             # Defaults
             if not "max_count" in search_params:
@@ -75,6 +86,15 @@ def search():
             if search_delete:
                 events_collection.remove(search_request)
                 text = "Events deleted"
+            elif get_types:
+                types = events_collection.distinct(key='event_type', filter={"$and": [
+                                    {"time": {"$gte": int(search_params['search']["time"]["from"])}},
+                                    {"time": {"$lte": int(search_params['search']["time"]["to"])}}
+                ]})
+                for type in types:
+                    results.append(type)
+                results_count = len(results)
+                text = "Types found"
             else:
                 search_results = events_collection.find(search_request).skip(int(search_params['skip'])).limit(int(search_params['max_count']))
                 all_results_count = search_results.count()
